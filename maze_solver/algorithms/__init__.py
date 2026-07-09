7f3b62cf21d4e79020f458fcc9b4c5e9ffe5bb5e
erraii <eraycakiray@gmail.com>
2026-07-09 20:21:10 +0200

    Initialize algorithms module with solver imports

---
+++ maze_solver/algorithms/__init__.py
@@ -0,0 +1,12 @@
+from .bfs import solve_bfs
+from .dfs import solve_dfs
+from .dijkstra import solve_dijkstra
+
+from .astar import solve_astar
+
+__all__ = [
+    "solve_bfs",
+    "solve_dfs",
+    "solve_dijkstra",
+    "solve_astar",
+]
